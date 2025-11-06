3c902936cc1b7d0fe8dbaf6a684ff5d02489d18d
Wysper <wysperotaku@gmail.com>
2025-11-06 14:44:47 +0100

    Refactor to the non generic interfaces

--- src/model/repository/UserDAO.php
+++
@@ -1,9 +0,0 @@
-<?php 
-interface UserDAO {
-    public function createUser($user);
-    public function updateUser($user, $newUser);
-    public function deleteUser($user);
-    public function getAllUsers();
-    public function getUserById($id);
-}
-?>
