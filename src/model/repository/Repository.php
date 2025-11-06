4f4a61801b71b6b0fbd71cd3eda7145126163a07
Wysper <wysperotaku@gmail.com>
2025-11-06 14:42:10 +0100

    Adding Generic interface for all repo interfaces

---
+++ src/model/repository/Repository.php
@@ -0,0 +1,36 @@
+<?php
+    /**
+     * @template T of object
+     */
+    interface Repository {
+        /**
+         * @param T $entity
+         * @return void
+         */
+        public function create($entity);
+
+        /**
+         * @param T $entity
+         * @param T $newEntity
+         * @return void
+         */
+        public function update($entity, $newEntity);
+
+        /**
+         * @param T $entity
+         * @return void
+         */
+        public function delete($entity);
+
+        /**
+         * @return array
+         */
+        public function getAll() : array;
+
+        /**
+         * @param T $entity
+         * @return T|null
+         */
+        public function get($entity);
+    }
+?>
