8cde7f4dd24e8de50178c7c65759a686bcb7cf0a
Wysper <wysperotaku@gmail.com>
2025-11-12 11:42:33 +0100

    Recomendation refactoring

--- src/model/entities/Recomendation.php
+++ src/model/entities/Recomendation.php
@@ -1,27 +1,57 @@
 <?php
 class Recomendation {
-    private $id;
-    private $user_id;
-    private $title;
-    private $description;
-    private $image;
+    private int $id;
+    private int $user_id;
+    private string $title;
+    private string $description;
+    private string $imageUrl;
 
-    public function __construct($id, $user_id, $title, $description, $image) {
+    public function __construct() {}
+
+    public function hydrate(array $data) : void {
+        foreach ($data as $k => $v) {
+            if(property_exists($this, $k)) {
+                $this->$k = $v;
+            }
+        }
+    }
+
+    public function init(int $id, int $user_id, string $title, string $description, string $imageUrl) : void {
         $this->id = $id;
         $this->user_id = $user_id;
         $this->title = $title;
         $this->description = $description;
-        $this->image = $image;
+        $this->imageUrl = $imageUrl;
     }
 
-    public function getId() { return $this->id; }
-    public function getUserId() { return $this->user_id; }
-    public function getTitle() { return $this->title; }
-    public function getDescription() { return $this->description; }
-    public function getImage() { return $this->image; }
+    public function getId() : int  { 
+        return $this->id; 
+    }
+
+    public function getUserId() : int { 
+        return $this->user_id; 
+    }
+
+    public function getTitle() : string { 
+        return $this->title; 
+    }
+
+    public function getDescription() : string { 
+        return $this->description; 
+    }
+
+    public function getImageUrl() : string { 
+        return $this->imageUrl; 
+    }
 
     public function __toString() {
-        return "Recomendation [ID: {$this->id}, UserId: {$this->user_id}, Title: {$this->title}, Description: {$this->description}, Image: {$this->image}]";
+        return "{
+                    id: {$this->id},
+                    userId: {$this->user_id},
+                    title: {$this->title},
+                    description: {$this->description},
+                    ImageUrl: {$this->imageUrl}
+                }";
     }
 }
 ?>
