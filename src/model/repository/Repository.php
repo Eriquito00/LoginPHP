8ee583aadc9ebd1011f8ccdedcdc9f039a821606
Wysper <wysperotaku@gmail.com>
2025-11-13 15:54:28 +0100

    Using namespaces and autoloader

--- src/model/repository/Repository.php
+++ src/model/repository/Repository.php
@@ -1,36 +1,39 @@
 <?php
+
+namespace App\Model\Repository;
+
+/**
+ * @template T of object
+ */
+interface Repository {
     /**
-     * @template T of object
+     * @param T $entity
+     * @return void
      */
-    interface Repository {
-        /**
-         * @param T $entity
-         * @return void
-         */
-        public function create($entity);
+    public function create($entity);
 
-        /**
-         * @param T $entity
-         * @param T $newEntity
-         * @return void
-         */
-        public function update($entity, $newEntity);
+    /**
+     * @param T $entity
+     * @param T $newEntity
+     * @return void
+     */
+    public function update($entity, $newEntity);
 
-        /**
-         * @param T $entity
-         * @return void
-         */
-        public function delete($entity);
+    /**
+     * @param T $entity
+     * @return void
+     */
+    public function delete($entity);
 
-        /**
-         * @return array
-         */
-        public function getAll() : array;
+    /**
+     * @return array
+     */
+    public function getAll() : array;
 
-        /**
-         * @param T $entity
-         * @return T|null
-         */
-        public function get($entity);
-    }
+    /**
+     * @param T $entity
+     * @return T|null
+     */
+    public function get($entity);
+}
 ?>
