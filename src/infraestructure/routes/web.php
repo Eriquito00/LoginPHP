acfde578734a98252740a97e606889bdd5db1117
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-11-17 12:45:16 +0100

    Refactor Router namespace and enhance dispatch method for dynamic routing

--- src/infraestructure/routes/web.php
+++ src/infraestructure/routes/web.php
@@ -1,3 +1,15 @@
-<?php 
-// FUTURAS RUTAS
+<?php
+
+use App\infraestructure\routes\Router;
+
+use App\Controller\HomeController;
+use App\Controller\LoginController;
+use App\Controller\RegisterController;
+
+Router::get("/", [HomeController::class, "index"]);
+
+Router::get("/login", [LoginController::class, "index"]);
+
+Router::get("/register", [RegisterController::class, "index"]);
+
 ?>
