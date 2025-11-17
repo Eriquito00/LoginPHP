2a27c29403d2cca99a162c79e4a015fec44d6afc
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-11-17 12:45:51 +0100

    Add Home, Login, and Register controllers; remove legacy controller files

--- src/controller/login_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once(__DIR__ . "/../view/login.php");
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        $email = $_POST["email"] ?? "";
-        $password = $_POST["passwd"] ?? "";
-    }
-?>
--- src/controller/register_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once(__DIR__ . "/../view/register.php");
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        $email = $_POST["email"] ?? "";
-        $password = $_POST["passwd"] ?? "";
-        $repeat_passwd = $_POST["repeat_passwd"] ?? "";
-    }
-?>
