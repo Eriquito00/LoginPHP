68931284fc2f85bc9ec5887ad21639c91c29ecb7
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-11-28 13:22:40 +0100

    Bugfix de los botones de paginacion cuando no hay resultados

--- src/view/components/_pagination_buttons.php
+++ src/view/components/_pagination_buttons.php
@@ -10,9 +10,13 @@ if (isset($pageData)){
 
 ?>
 
-<div class='section_pagination_buttons'>
+<?php if (isset($pageData)): ?>
+<section class='section_pagination_buttons'>
+    <?php if ($totalItems <= 0): ?>
+        <h3>No se han encontrado resultados</h3>
+    <?php endif; ?>
     <div class='buttons'>
-        <button id="page" value="<?= $actualPage - 1 ?>" class="btn_filter <?= !$hasPrev ?  "no_clickable" : ""?>"><</button>
+        <button id="page" value="<?= $actualPage - 1 ?>" class="btn_filter <?= !$hasPrev || $totalItems <= 0 ?  "no_clickable" : ""?>"><</button>
 
         <?php if ($actualPage > 2): ?>
             <button id="page" value="<?= 1 ?>" class="btn_filter">1</button>
@@ -21,10 +25,16 @@ if (isset($pageData)){
             <button class="btn_filter no_clickable">...</button>
         <?php endif; ?>
 
-        <?php if ($hasPrev): ?>
+        <?php if ($hasPrev && $totalItems > 0): ?>
             <button id="page" value="<?= $actualPage - 1 ?>" class="btn_filter"><?= $actualPage - 1 ?></button>
         <?php endif; ?>
-        <button id="page" value="<?= $actualPage ?>" class="btn_filter actual_page no_clickable"><?= $actualPage ?></button>
+
+        <?php if ($totalItems > 0): ?>
+            <button id="page" value="<?= $actualPage ?>" class="btn_filter actual_page no_clickable"><?= $actualPage ?></button>
+        <?php elseif ($totalItems <= 0): ?>
+            <button id="page" value="<?= 1 ?>" class="btn_filter actual_page no_clickable">1</button>
+        <?php endif; ?>
+
         <?php if ($hasNext): ?>
             <button id="page" value="<?= $actualPage + 1 ?>" class="btn_filter"><?= $actualPage + 1 ?></button>
         <?php endif; ?>
@@ -39,4 +49,5 @@ if (isset($pageData)){
         <button id="page" value="<?= $actualPage + 1 ?>" class="btn_filter <?= !$hasNext ?  "no_clickable" : ""?>">></button>
     </div>
     <p>Total items: <strong id='items_page' class='btn_filter'><?= $totalItems ?></strong></p>
-</div>
+</section>
+<?php endif; ?>
