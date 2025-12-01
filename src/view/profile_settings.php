d43bdf3e8f5943e5488b16c3081aad3d4020a513
Eric Mejias Gamonal <ericmejiasgamonal@gmail.com>
2025-12-01 18:42:23 +0100

    Aplicado los componentes a otras paginas

--- src/view/profile_settings.php
+++ src/view/profile_settings.php
@@ -10,79 +10,72 @@
     <link rel="stylesheet" href="./styles/profile_settings.css">
 </head>
 <body>
-    <h1>Opciones de tu perfil</h1>
-    <nav>
-        <ul>
-            <li><a href=""><button>Home</button></a></li>
-            <li><a href="login"><button>Login</button></a></li>
-            <li><a href="register"><button>Register</button></a></li>
-            <li><a href="profile"><button>Profile</button></a></li>
-            <li><a href="profile/settings"><button>Profile/Settings</button></a></li>
-            <li><a href="profile/create"><button>Create Reco</button></a></li>
-            <li><a href="login/forgot-password"><button>Passoword</button></a></li>
-            <li><a href="admin/users"><button>Admin Users</button></a></li>
-        </ul>
-    </nav>
-    <section>
-        <form method="POST" action="profile/settings">
-            <div class="div_img_profile">
-                <div class="img_profile">
-                    <img class="img_profile" src="./assets/moai.jpg" alt="Foto de perfil">
-                    <label for="image">Foto de perfil</label>
+    <?php require_once(__DIR__ . "/components/_header.php"); ?>
+
+    <?php require_once(__DIR__ . "/components/_navigation.php"); ?>
+
+    <main id="main_content" class="main_content">
+        <section>
+            <form method="POST" action="profile/settings">
+                <div class="div_img_profile">
+                    <div class="img_profile">
+                        <img class="img_profile" src="./assets/moai.jpg" alt="Foto de perfil">
+                        <label for="image">Foto de perfil</label>
+                    </div>
+                    <div>
+                        <input name="image" type="file">
+                        <button class="btn-form" type="submit">Aplicar</button>
+                    </div>
                 </div>
+                <input type="hidden" name="action" value="changePhoto">
+            </form>
+
+            <form method="POST" action="profile/settings" class="div-structure">
+                <label for="username">Username</label>
+                <p>Cambia tu nombre de usuario y pulsa <strong>renombrar</strong> para cambiarlo.</p>
+                <div class="div-1">
+                    <input name="username" type="text" placeholder="Username">
+                    <button class="btn-form" type="submit">Renombrar</button>
+                </div>
+                <input type="hidden" name="action" value="changeUsername">
+            </form>
+
+            <form method="POST" action="profile/settings" class="div-structure">
+                <label for="email">Email</label>
+                <p>Introduce tu correo electronico y pulsa en <strong>verificar</strong>. Una vez hecho te llegara un correo electronico para la verificacion.</p>
+                <div class="div-1">
+                    <input name="email" type="email" placeholder="Email">
+                    <button class="btn-form" type="submit">Verificar</button>
+                </div>
+                <input type="hidden" name="action" value="verifyEmail">
+            </form>
+
+            <form method="POST" action="profile/settings" class="div-structure">
+                <label for="email">Email</label>
                 <div>
-                    <input name="image" type="file">
-                    <button class="btn-form" type="submit">Aplicar</button>
+                    <input name="email" type="email" placeholder="Email">
                 </div>
-            </div>
-            <input type="hidden" name="action" value="changePhoto">
-        </form>
+                <label for="newEmail">Nuevo Email</label>
+                <div>
+                    <input name="newEmail" type="email" placeholder="Nuevo Email">
+                </div>
+                <button class="btn-form" type="submit">Cambiar</button>
+                <input type="hidden" name="action" value="changeEmail">
+            </form>
 
-        <form method="POST" action="profile/settings" class="div-structure">
-            <label for="username">Username</label>
-            <p>Cambia tu nombre de usuario y pulsa <strong>renombrar</strong> para cambiarlo.</p>
-            <div class="div-1">
-                <input name="username" type="text" placeholder="Username">
-                <button class="btn-form" type="submit">Renombrar</button>
-            </div>
-            <input type="hidden" name="action" value="changeUsername">
-        </form>
-
-        <form method="POST" action="profile/settings" class="div-structure">
-            <label for="email">Email</label>
-            <p>Introduce tu correo electronico y pulsa en <strong>verificar</strong>. Una vez hecho te llegara un correo electronico para la verificacion.</p>
-            <div class="div-1">
-                <input name="email" type="email" placeholder="Email">
-                <button class="btn-form" type="submit">Verificar</button>
-            </div>
-            <input type="hidden" name="action" value="verifyEmail">
-        </form>
-
-        <form method="POST" action="profile/settings" class="div-structure">
-            <label for="email">Email</label>
-            <div>
-                <input name="email" type="email" placeholder="Email">
-            </div>
-            <label for="newEmail">Nuevo Email</label>
-            <div>
-                <input name="newEmail" type="email" placeholder="Nuevo Email">
-            </div>
-            <button class="btn-form" type="submit">Cambiar</button>
-            <input type="hidden" name="action" value="changeEmail">
-        </form>
-
-        <form method="POST" action="profile/settings" class="div-structure">
-            <label for="password">Password</label>
-            <div>
-                <input name="password" type="password" minlength="8" placeholder="Password">
-            </div>
-            <label for="newPassword">Nueva Password</label>
-            <div>
-                <input name="newPassword" type="password" minlength="8" placeholder="Nueva Password">
-            </div>
-            <button class="btn-form" type="submit">Cambiar</button>
-            <input type="hidden" name="action" value="changePassword">
-        </form>
-    </section>
+            <form method="POST" action="profile/settings" class="div-structure">
+                <label for="password">Password</label>
+                <div>
+                    <input name="password" type="password" minlength="8" placeholder="Password">
+                </div>
+                <label for="newPassword">Nueva Password</label>
+                <div>
+                    <input name="newPassword" type="password" minlength="8" placeholder="Nueva Password">
+                </div>
+                <button class="btn-form" type="submit">Cambiar</button>
+                <input type="hidden" name="action" value="changePassword">
+            </form>
+        </section>
+    </main>
 </body>
 </html>
