6acd795633615b5ce94a0067c528f62ea9d6d0f5
Wysper <wysperotaku@gmail.com>
2025-12-02 19:45:29 +0100

    Enhance login form structure by adding missing IDs for input fields and correcting form action URL for better functionality

--- src/view/login.php
+++ src/view/login.php
@@ -15,22 +15,23 @@ if (!isset($_SESSION["login_try"])){
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <link rel="stylesheet" href="./styles/loginregister.css">
+    <script type="module" src="./js/LoginFormData.js"></script>
 </head>
 <body>
-    <form method="POST" action="login">
+    <form method="POST" id="loginForm" action="auth/login">
         <h1>Login</h1>
         <div>
             <label for="email">Email</label>
-            <input name="email" type="email" placeholder="Introduce tu email" required>
+            <input name="email" id="email" type="email" placeholder="Introduce tu email" required>
         </div>
 
         <div>
             <label for="email">Contraseña</label>
-            <input name="password" type="password" minlength="8" placeholder="Introduce tu contraseña" required>
+            <input name="password" id="password" type="password" minlength="8" placeholder="Introduce tu contraseña" required>
         </div>
         <div class="rembember div_rem_passwd">
             <div class="rembember">
-                <input name="remember" type="checkbox">
+                <input name="remember" id="remember" type="checkbox">
                 <label for="remember">Remember me</label>
             </div>
             <a class="forgot_passwd" href="login/forgot-password-data">Has olvidado la contraseña?</a>
