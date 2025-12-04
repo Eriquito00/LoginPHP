6ce237aca9622de1e0b0a8186e5f40a415d1013f
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-12-04 16:34:01 +0100

    Nueva constante necesaria

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ $dir = str_replace('\\', '/', __DIR__);
 $basePath = str_replace($documentRoot, '', $dir);
 $host = $_SERVER["HTTP_HOST"];
 define('BASE_URL', "http://$host$basePath/");
+//UNA NUEVA CONSTANTE CON LA RUTA HASTA ANTES DE PUBLIC
 
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../src/infraestructure/routes/web.php";
